a61550401fa4f20f711b2f5243a3c53ef66317ed
Vojin <karisik1.vojin3@gmail.com>
2024-10-08 09:08:13 +0200

    42 Seeding Database  and Fetching Users

---
+++ src/app/members/page.tsx
@@ -0,0 +1,13 @@
+import { getMembers } from "../actions/memberActions";
+
+export default async function MembersPage() {
+  const members = await getMembers();
+  return (
+    <div>
+      <ul>
+        {members &&
+          members.map((member) => <li key={member.id}>{member.name}</li>)}
+      </ul>
+    </div>
+  );
+}
