ca36c0892803fde07ae9f69f5912045602a7b224
Vojin <karisik1.vojin3@gmail.com>
2024-10-11 23:40:00 +0200

    89 Messages Page Functionality Setup

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,3 +1,18 @@
-export default function MessagesPage() {
-  return <div></div>;
+import { getMessagesByContainer } from "../actions/messageActions";
+import MessageSidebar from "./MessageSidebar";
+
+export default async function MessagesPage({
+  searchParams,
+}: {
+  searchParams: { container: string };
+}) {
+  const messages = await getMessagesByContainer(searchParams.container);
+  return (
+    <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
+      <div className="col-span-2">
+        <MessageSidebar />
+      </div>
+      <div className="col-span-10">Message table goes here</div>
+    </div>
+  );
 }
